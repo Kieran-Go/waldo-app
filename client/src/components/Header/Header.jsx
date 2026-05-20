51f06244441dc8300954776f2f86c00f2d79eb8d
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2026-05-20 13:14:19 +0930

    Implement Header with navigation and styles
    - Added logo and title to header component
    - Wrapped header content in home navigation link
    - Imported waldo-logo.png for use in header
    - Styled Header.css with color, flexbox and sticky position

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import './Header.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/waldo-logo.png';
 
 export default function Header(){
     // ----- RENDER -----
   return(
     <header>
-        <h1>Hello, World. This is a Header!</h1>
+        <Link to='/'>
+          <img src={logo} alt="Where's Waldo site logo"/>
+          <h1>Where's <span>Waldo?</span></h1>
+        </Link>
     </header>
   );
 }
