0a2ee7044a4f8dc89485ae36ce4acd581b02402f
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2026-05-22 07:51:32 +0930

    Add scene selection grid to Home page
    - Create temporary scene-data.js mock data file
    - Render scene cards dynamically from scene data
    - Add navigation to game routes with Link
    - Display scene thumbnails and names on homepage
    - Create grid layout for scene cards
    - Style scene grid

---
+++ client/src/mock_data/scene-data.js
@@ -0,0 +1,35 @@
+// Game scene mock-data to use for creating the UI but to be eventually replaced by data fetched from backend API
+const scenes = [
+    {
+        id: 0,
+        name: "A Dream Come True",
+        image: "https://i.imgur.com/pk88M60.jpeg"
+    },
+    {
+        id: 1,
+        name: "A Dream Come True",
+        image: "https://i.imgur.com/pk88M60.jpeg"
+    },
+    {
+        id: 2,
+        name: "A Dream Come True",
+        image: "https://i.imgur.com/pk88M60.jpeg"
+    },
+    {
+        id: 3,
+        name: "A Dream Come True",
+        image: "https://i.imgur.com/pk88M60.jpeg"
+    },
+    {
+        id: 4,
+        name: "A Dream Come True",
+        image: "https://i.imgur.com/pk88M60.jpeg"
+    },
+    {
+        id: 5,
+        name: "A Dream Come True",
+        image: "https://i.imgur.com/pk88M60.jpeg"
+    },
+];
+
+export default scenes;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,28 @@
 import Header from '../../components/Header/Header';
+import scenes from '../../mock_data/scene-data';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 export default function Home(){
   // ----- RENDER -----
   return(
-    <Header />
+    <>
+      {/* Render Header component */}
+      <Header />
+
+      <div className='home'>
+        {/* Render scenes */}
+        <section className="scenes-container">
+            {/* Map through each scene */}
+            {scenes.map((s) => (
+                // Render scene-card
+                <Link className="scene-card" key={s.id} to={`/game/${s.id}`}>
+                    <img src={s.image} alt={`Thumbnail image for ${s.name}`}/>
+                    <h3>{s.name}</h3>
+                </Link>
+            ))}
+        </section>
+      </div>
+    </>
   );
 }
