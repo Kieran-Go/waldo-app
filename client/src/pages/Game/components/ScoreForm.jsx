644f85eb0a53830585fc87ba1313090b067935bf
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2026-06-05 08:58:18 +0930

    Add score submission form
    - Replace placeholder ScoreForm with interactive submission form
    - Add form submit handler with navigation to leaderboard
    - Apply styling for score form UI
    - Add new variable into variables.css

--- client/src/pages/Game/components/ScoreForm.jsx
+++ client/src/pages/Game/components/ScoreForm.jsx
@@ -1,11 +1,53 @@
+import "./ScoreForm.css";
 import secondsToMinutes from "../../../utils/secondsToMinutes";
+import BlackOverlay from "../../../components/BlackOverlay/BlackOverlay";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function ScoreForm({ timer, gameId }) {
+    // ----- HOOKS ------
+    const displayTimer = secondsToMinutes(timer);
+    const nav = useNavigate();
+    const [name, setName] = useState('');
+
+    // ----- FUNCTIONS -----
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // TODO: POST score data to backend DB
+        try{
+            // Navigate to leaderboard page after POST
+            nav("/leaderboard");
+        }
+        catch{
+            alert("An error occurred while submitting your score.");
+        }
+    }
+
     // ----- RENDER -----
     return(
-        // Placeholder
-        <div className="score-form">
-            <p>Game {gameId} completed at {secondsToMinutes(timer)}</p>
-        </div>
+        <>
+            <BlackOverlay zIndex={1001} fadeIn={true} fadeDuration={300} />
+            <div className="form-container">
+                <h2>You found all characters!</h2>
+                <p>Your time: <span>{displayTimer}</span></p>
+                <p>Enter a name to submit to the leaderboard:</p>
+
+                <form onSubmit={handleSubmit} role="form">
+                    <input
+                        type="text"
+                        placeholder="Enter your name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value.slice(0, 30))}
+                        required
+                        maxLength={30}
+                    />
+                    <button type="submit">Submit Score</button>
+                </form>
+
+                <Link to="/" className="home-button">Back to Home</Link>
+            </div>
+        </>
     );
 }
